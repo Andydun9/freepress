177154d62e3eccafc35529340cca563e8603507d
Andydun9 <33610842+Andydun9@users.noreply.github.com>
2021-11-22 16:03:32 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// navigational buttons
+
+function politics(){
+    document.getElementById("title").innerHTML="Politics" 
+}
+
+function finance(){
+    document.getElementById("title").innerHTML="Finance" 
+}
+
+function sports(){
+    document.getElementById("title").innerHTML="Sports" 
+}
+
+function pop(){
+    document.getElementById("title").innerHTML="Pop Culture &amp Music" 
+}
+
+function satire(){
+    document.getElementById("title").innerHTML="Satire" 
+}
